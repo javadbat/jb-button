406c916aeb0911aad4679700aa6565720a58c0cf
javadbat <javadbat@gmail.com>
2025-02-01 15:37:43 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,23 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-button",
+    path: "./lib/jb-button.ts",
+    outputPath: "./dist/jb-button.js",
+    external: [],
+    umdName: "JBButton",
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-button-react",
+    path: "./react/lib/JBButton.tsx",
+    outputPath: "./react/dist/JBButton.js",
+    external: ["jb-button", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
